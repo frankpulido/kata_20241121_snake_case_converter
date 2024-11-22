1ffd34922f837a3d7aa1cddf2bbfee80ca10ea1d
frankpulido <frankpulido@me.com>
2024-11-22 19:43:16 +0100

    only second trim was needed, removed first

--- snake_case.php
+++ snake_case.php
@@ -5,8 +5,7 @@ class SnakeCaseConverter {
 
     public function MakeSnake($string) : string {
         $snake = "";
-        $snake = trim($string);
-        $snake = str_replace("ñ","n",mb_strtolower($snake));
+        $snake = str_replace("ñ","n",mb_strtolower($string));
         $snake = preg_replace("/[^a-zA-Z0-9 ]/", "", $snake);
         // Above we filter special characters (pregreplace) accepting only A-Za-z0-9 and empty spaces.
         // $snake = str_replace(" ","_", $snake); // Discarded. The reason : This command replaces white spaces for underscores but may return multiple underscores concatenated
